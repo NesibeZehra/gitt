694956e686368b2983116d5c0b07c24bad07f0e4
Nesibe Zehra Atay <nesibeatay@gmail.com>
2024-05-26 20:15:00 +0300

    v1

---
+++ src/main/java/day1/Klon1.java
@@ -0,0 +1,12 @@
+package day1;
+
+public class Klon1 {
+    public static void main(String[] args) {
+
+//versiyon1
+        System.out.println("hello");
+        System.out.println("my");
+
+
+    }
+}
